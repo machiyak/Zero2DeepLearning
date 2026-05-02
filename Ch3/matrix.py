fe1c1a4b784689c207fa7e959da55e892b0065e3
machiyak <day.by.day4692@icloud.com>
2026-05-02 22:04:19 +0900

    implemment 3-Stage NN

---
+++ Ch3/matrix.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def sigmoid_function(x):
+    return 1/(1 + np.exp(-x))
+
+def identity_function(x):
+    return x
+
+# First Stage
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+
+A1 = np.dot(X, W1) + B1
+Z1 = sigmoid_function(A1)
+
+# Second Stage
+W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+B2 = np.array([0.1, 0.2])
+
+A2 = np.dot(Z1, W2) + B2
+Z2 = sigmoid_function(A2)
+
+# Third Stage
+W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+B3 = np.array([0.1, 0.2])
+
+A3 = np.dot(Z2, W3) + B3
+Y = identity_function(A3)
+
+print(Y)
