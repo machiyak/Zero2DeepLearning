c00d2f9f8f2728744d2fdbe5f6f479388dad95bb
machiyak <day.by.day4692@icloud.com>
2026-05-07 15:32:20 +0900

    implement numerical dofferentiation & show example

---
+++ Ch4/differentiation.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def numerical_diff(f, x):
+    h = 1e-4
+    return ((f + h) - (f - h)) / (2 * h)
+
+# y = 0.01x^2 + 0.1x
+def function_1(x):
+    return 0.01 * x**2 + 0.1 * x
+
+x = np.arange(0.0, 20.0, 0.1)
+y = function_1(x)
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.plot(x, y)
+plt.show()
