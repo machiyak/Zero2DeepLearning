94a0ae15422563c7a9a13db67d43bc96ae6d212a
machiyak <day.by.day4692@icloud.com>
2026-05-07 15:41:39 +0900

    displayed the differentiated results

--- Ch4/differentiation.py
+++ Ch4/differentiation.py
@@ -3,15 +3,26 @@ import matplotlib.pyplot as plt
 
 def numerical_diff(f, x):
     h = 1e-4
-    return ((f + h) - (f - h)) / (2 * h)
+    return (f(x + h)- f(x - h)) / (2 * h)
 
 # y = 0.01x^2 + 0.1x
 def function_1(x):
     return 0.01 * x**2 + 0.1 * x
 
+def tangent_line(f, x):
+    d = numerical_diff(f, x)
+    print(d)
+    y = f(x) - d*x
+    return lambda t: d*t + y
+
 x = np.arange(0.0, 20.0, 0.1)
 y = function_1(x)
 plt.xlabel("x")
 plt.ylabel("f(x)")
+
+tf = tangent_line(function_1, 5)
+y2 = tf(x)
+
 plt.plot(x, y)
+plt.plot(x, y2)
 plt.show()
